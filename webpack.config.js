3d1a4a0e3199eacc692422c4be32cb615fce0ffc
KKambi <37494867+KKambi@users.noreply.github.com>
2019-09-17 10:59:23 +0900

    Update: module (webpack)
    - 번들 파일에서 CSS 파일을 추출하고 병렬로 로딩할 수 있도록 처리
    - webpack MiniCssExtractPlugin을 이용

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,34 @@
 //참고: https://ui.toast.com/fe-guide/ko_BUNDLER/
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/entry.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
-    }
+  entry: './public/entry.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // fallback to style-loader in development
+          process.env.NODE_ENV !== 'production' 
+            ? 'style-loader'
+            : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ]
 }
