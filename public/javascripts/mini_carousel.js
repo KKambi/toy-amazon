62e504518ab92555d770c7d129fcb389c68c6d64
KKambi <37494867+KKambi@users.noreply.github.com>
2019-09-18 16:41:51 +0900

    Simplify: mini_carousel duplicate code to function
    - 미니 카로셀 UI에서 중복되는 작업들을 함수로 추출
    - 클래스로 추상화 작업 진행중

--- public/javascripts/mini_carousel.js
+++ public/javascripts/mini_carousel.js
@@ -1,7 +1,4 @@
-const leftArrow = document.getElementById("left-arrow")
-const rightArrow = document.getElementById("right-arrow")
-const miniCarouselRow = document.querySelector(".mini-carousel-row")
-const button = document.querySelector(".mini-carousel-arrow")
+//Object For Indexing
 const childMap = {
     "left": "lastChild",
     "right": "firstChild"
@@ -10,55 +7,110 @@ const insertMap = {
     "left": "afterbegin",
     "right": "beforeend"
 }
+
+//Variable for State
 let intervalId = ""
 let direction = ""
 
-miniCarouselRow.addEventListener("transitionend", () => {
+//DOM Element
+const leftArrow = document.getElementById("left-arrow")
+const rightArrow = document.getElementById("right-arrow")
+const miniCarouselRow = document.querySelector(".mini-carousel-row")
+const button = document.querySelector(".mini-carousel-arrow")
+
+class MiniCarousel {
+    constructor(){
+        this.intervalId = ""
+        this.direction = ""
+        this.elements = {
+            leftArrow: document.getElementById("left-arrow"),
+            rightArrow: document.getElementById("right-arrow"),
+            miniCarouselRow: document.querySelector(".mini-carousel-row"),
+            button: document.querySelector(".mini-carousel-arrow")
+        }
+    }
+}
+
+/** 
+ * Slide carousel card to left/right
+ *
+ * @param {string} direction sliding direction ["left"/"right"]
+ * @return {void} 
+ */
+const slideCard = function(direction){
+    const translate = miniCarouselRow.style.transform
+    const currentSign = translate.includes('-') ? -1 : 1
+    const currentPixel = Number(translate.replace(/[^0-9]/g,''))
+    const slidePixel = direction==="left" ? 280 : -280
+
+    miniCarouselRow.style.transition = 'all 0.1s ease-out'
+    miniCarouselRow.style.transform = `translateX(${currentSign * currentPixel + slidePixel}px)`
+}
+
+/** 
+ * Move carousel [first/last] card to [last/first] 
+ *
+ * @param {string} direction sliding direction ["left"/"right"]
+ * @return {void} 
+ */
+const moveCard = function(direction){
     const card = miniCarouselRow[childMap[direction]]
     miniCarouselRow.removeChild(card)
     miniCarouselRow.insertAdjacentElement(insertMap[direction], card)
     miniCarouselRow.style.transition = ''
     miniCarouselRow.style.transform = `translateX(${-280}px)`
-    button.removeAttribute("disabled")
-})
+}
 
+/** 
+ * Start Auto Slide card to right
+ *
+ * @param {string} interval sliding interval (ms)
+ * @return {number} intervalId 
+ */
+const startAutoSlide = function(interval){
+    return setInterval(() => {
+        slideCard("right")
+        button.setAttribute("disabled", "")
+    }, interval)
+}
+
+//Auto sliding start
+intervalId = startAutoSlide(3000)
+
+//Auto sliding restart
+setInterval(() => {
+    if (intervalId === ""){
+        // sliding start
+        direction = "right"
+        intervalId = startAutoSlide(3000)
+        button.setAttribute("disabled", "")
+    }
+}, 10000)
+
+//Carousel left-arrow click event
 leftArrow.addEventListener("click", () => {
     if (intervalId){
         clearInterval(intervalId)
         intervalId = ""
     }
-    const translate = miniCarouselRow.style.transform
-    const px = Number(translate.replace(/[^0-9]/g,''))
-    const sign = translate.includes('-') ? -1 : 1
     direction = "left"
-
-    miniCarouselRow.style.transition = 'all 0.1s ease-out'
-    miniCarouselRow.style.transform = `translateX(${px * sign + 280}px)`
+    slideCard(direction)
     button.setAttribute("disabled", "")
 })
 
+//Carousel right-arrow click event
 rightArrow.addEventListener("click", () => {
     if (intervalId){
         clearInterval(intervalId)
         intervalId = ""
     }
-    const translate = miniCarouselRow.style.transform
-    const px = Number(translate.replace(/[^0-9]/g,''))
-    const sign = translate.includes('-') ? -1 : 1
     direction = "right"
-
-    miniCarouselRow.style.transition = 'all 0.1s ease-out'
-    miniCarouselRow.style.transform = `translateX(${px * sign - 280}px)`
+    slideCard(direction)
     button.setAttribute("disabled", "")
 })
 
-intervalId = setInterval(() => {
-    const translate = miniCarouselRow.style.transform
-    const px = Number(translate.replace(/[^0-9]/g,''))
-    const sign = translate.includes('-') ? -1 : 1
-
-    direction = "right"
-    miniCarouselRow.style.transition = 'all 0.1s ease-out'
-    miniCarouselRow.style.transform = `translateX(${px * sign - 280}px)`
-    button.setAttribute("disabled", "")
-}, 3000)
+//Carousel card transition end event
+miniCarouselRow.addEventListener("transitionend", () => {
+    moveCard(direction)
+    button.removeAttribute("disabled")
+})
