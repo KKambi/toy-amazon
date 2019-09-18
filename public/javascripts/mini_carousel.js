712ffe4b771705e6c8632d4607e7a383262b2854
KKambi <37494867+KKambi@users.noreply.github.com>
2019-09-18 19:01:01 +0900

    Fix: insert html dynamically in MiniCarousel
    - 동적으로 html을 삽입하도록 변경
    - 동적 삽입 시, 이벤트가 붙지 않는 문제 해결 필요
    - Event delegation을 이용하여 상위 요소에서 이벤트를 처리하도록
    수정해야함

--- public/javascripts/mini_carousel.js
+++ public/javascripts/mini_carousel.js
@@ -2,11 +2,16 @@ import { is_util } from './is_util.js'
 import { dom_util } from './dom_util.js'
 
 const WIDTH = 280
-const HEIGHT = 210 
+const HEIGHT = 210
 const SECOND_3 = 3000
 
 class MiniCarousel {
-    constructor(imageWidth, imageHeight, interval){
+    constructor(imageWidth, imageHeight, interval) {
+        this.html = `
+            <div class="mini-carousel-scroller">
+                <div class="mini-carousel-container">
+                    <div class="mini-carousel-col mini-carousel-left"> 
+                        <a class="mini-carousel-arrow" id="left-arrow"></a></div><div class="mini-carousel-col mini-carousel-center"><div class="mini-carousel-viewport"><ol class="mini-carousel-row" style="transform: translateX(-280px);"><li class="mini-carousel-card"><a class="img-link"><img src="/images/Sub_Card_A/Sub_Card_A01.jpg" alt="Homecoming New Series"></a></li><li class="mini-carousel-card"><a class="img-link"><img src="/images/Sub_Card_A/Sub_Card_A02.jpg" alt="The Man in the High Castle"></a></li><li class="mini-carousel-card"><a class="img-link"><img src="/images/Sub_Card_A/Sub_Card_A03.jpg" alt="The Marvelous Mrs.Meisel"></a></li><li class="mini-carousel-card"><a class="img-link"><img src="/images/Sub_Card_A/Sub_Card_A04.jpg" alt="Tom Clansy's Jack Lyan"></a></li></ol></div></div><div class="mini-carousel-col mini-carousel-right"><a class="mini-carousel-arrow" id="right-arrow"></a></div></div></div>`
         this.imageWidth = imageWidth || 300
         this.imageHeight = imageHeight || 300
         this.interval = interval || SECOND_3
@@ -30,43 +35,50 @@ class MiniCarousel {
         }
     }
 
-    init(){
+    getHTML(){
+        return this.html
+    }
+
+    init(container) {
         //Start Auto Sliding
-        this.intervalId = this.startAutoSlide(this.interval)    
+        this.intervalId = this.startAutoSlide(this.interval)
 
         //Check State Cyclically -> Restart Auto sliding
         setInterval(() => {
-            if (is_util.isNotWorking(this.intervalId)){
+            if (is_util.isNotWorking(this.intervalId)) {
                 this.intervalId = this.startAutoSlide(this.interval)
                 dom_util.disableElement(this.elements.button)
             }
         }, this.interval * 4)
 
         //Add arrow click event handler
-        this.addArrowEventHandler(this.elements.leftArrow, "left")
-        this.addArrowEventHandler(this.elements.rightArrow, "right")
+        // this.addArrowEventHandler(this.elements.leftArrow, "left")
+        // this.addArrowEventHandler(this.elements.rightArrow, "right")
+        this.addArrowEventHandler(container, "left")
+        // this.addArrowEventHandler(container, "right")
 
         //Add carousel card transition end event handler
-        this.addTransitionEndEventHandler(this.elements.miniCarouselRow)
+        // this.addTransitionEndEventHandler(this.elements.miniCarouselRow)
+        this.addTransitionEndEventHandler(container)
     }
 
     /** Set direction */
-    setDirection(direction){
-        this.direction = direction 
+    setDirection(direction) {
+        this.direction = direction
     }
 
     /** Initialize intervalId */
-    initializeIntervalId(){
+    initializeIntervalId() {
         this.intervalId = ""
     }
-   
+
     /** 
      * Start Auto Slide card to right
      *
      * @param {string} interval sliding interval (ms)
      * @return {number} intervalId 
      */
-    startAutoSlide(interval){
+    startAutoSlide(interval) {
         return setInterval(() => {
             this.setDirection("right")
             this.slideCard("right")
@@ -80,11 +92,11 @@ class MiniCarousel {
      * @param {string} direction sliding direction ["left"/"right"]
      * @return {void} 
      */
-    slideCard(direction){
+    slideCard(direction) {
         const miniCarouselRow = this.elements.miniCarouselRow
         const translate = miniCarouselRow.style.transform
         const currentSign = translate.includes('-') ? -1 : 1
-        const currentPixel = Number(translate.replace(/[^0-9]/g,''))
+        const currentPixel = Number(translate.replace(/[^0-9]/g, ''))
         const slidePixel = direction === "left" ? this.imageWidth : -this.imageWidth
 
         miniCarouselRow.style.transition = 'all 0.1s ease-out'
@@ -97,7 +109,7 @@ class MiniCarousel {
      * @param {string} direction sliding direction ["left"/"right"]
      * @return {void} 
      */
-    relocateCard(direction){
+    relocateCard(direction) {
         const card = this.elements.miniCarouselRow[this.map.childMap[direction]]
 
         this.elements.miniCarouselRow.removeChild(card)
@@ -112,15 +124,17 @@ class MiniCarousel {
      * @param {element} element arrow
      * @return {void}
      */
-    addArrowEventHandler(element, direction = "left"){
-        element.addEventListener("click", () => {
-            if (is_util.isWorking(this.intervalId)){
+    addArrowEventHandler(element, direction = "left") {
+        element.addEventListener("click", (event) => {
+            if (is_util.isWorking(this.intervalId)) {
                 clearInterval(this.intervalId)
                 this.initializeIntervalId()
             }
-            this.setDirection(direction)
-            this.slideCard(direction)
-            dom_util.disableElement(this.elements.button)
+            if (event.target && event.target.id == "left-arrow"){
+                this.setDirection(direction)
+                this.slideCard(direction)
+                dom_util.disableElement(this.elements.button)
+            }
         })
     }
 
@@ -130,12 +144,17 @@ class MiniCarousel {
      * @param {element} element carousel row
      * @return {void}
      */
-    addTransitionEndEventHandler(element){
+    addTransitionEndEventHandler(element) {
         element.addEventListener("transitionend", () => {
             this.relocateCard(this.direction)
             dom_util.enableElement(this.elements.button)
         })
-    }    
+    }
 }
 
-new MiniCarousel(WIDTH, HEIGHT, SECOND_3).init()
+const mini = new MiniCarousel(WIDTH, HEIGHT, SECOND_3)
+const container = document.getElementById("video-benefit-container")
+container.insertAdjacentHTML('afterbegin', mini.getHTML())
+mini.init(container)
+
+console.log(container)
