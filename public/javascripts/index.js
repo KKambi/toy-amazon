7bfa472c716a1913a4ea57694cc22aa6b3138686
KKambi <37494867+KKambi@users.noreply.github.com>
2019-09-18 10:23:29 +0900

    Fix: carousel button debouncing
    - 카로셀에서 좌우 버튼을 딜레이없이 누를시, carousel-row의 밀림이
    계속해서 일어나는 현상 수정

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,8 @@
 const leftArrow = document.getElementById("left-arrow")
 const miniCarouselRow = document.querySelector(".mini-carousel-row")
 const rMinus = /-/
-let click;
+// let click;
+let button = document.querySelector(".mini-carousel-arrow")
 
 miniCarouselRow.addEventListener("transitionend", () => {
     let lastCard = miniCarouselRow.lastChild
@@ -9,17 +10,14 @@ miniCarouselRow.addEventListener("transitionend", () => {
     miniCarouselRow.insertAdjacentElement('afterbegin', lastCard)
     miniCarouselRow.style.transition = ''
     miniCarouselRow.style.transform = `translateX(${-280}px)`
+    button.removeAttribute("disabled")
 })
 
 leftArrow.addEventListener("click", () => {
-    if (click) {
-        clearTimeout(click)
-    }
     let translate = miniCarouselRow.style.transform
     let px = Number(translate.replace(/[^0-9]/g,''))
     let sign = rMinus.test(translate) ? -1 : 1
-    click = setTimeout(() => {
-        miniCarouselRow.style.transition = 'all 0.3s ease-out'
-        miniCarouselRow.style.transform = `translateX(${px * sign + 280}px)`
-    }, 100)
+    miniCarouselRow.style.transition = 'all 0.3s ease-out'
+    miniCarouselRow.style.transform = `translateX(${px * sign + 280}px)`
+    button.setAttribute("disabled", "")
 })
