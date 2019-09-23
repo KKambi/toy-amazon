78d85d12dff9e4585322c30645045c9e9d37bb65
KKambi <37494867+KKambi@users.noreply.github.com>
2019-09-23 18:55:53 +0900

    Add: setting little of express-session
    - express-session의 사용을 위한 express설정
    - session option의 secret을 수정하다가 중단

--- routes/sessions.js
+++ routes/sessions.js
@@ -2,6 +2,25 @@
 const express = require('express');
 const router = express.Router();
 const session = require('express-session');
+const redis = require('redis')
+
+// import util
+const constant = require('../src/utils/constant.js')
+
+// Session setting
+let RedisStore = require('connect-redis')(session)
+let client = redis.createClient()
+app.use(session({
+    name: "amazon_session_id",
+    genid: function(req){
+        return uuid;    //uuid 라이브러리릍 통해 세션id 반환
+    },
+    store: new RedisStore({ client }),
+    secret: 'asadlfkj!@#!@#dfgasdg',    //TODO: hash를 위한 문자열이래
+    resave: true,
+    saveUninitialized: false,
+    cookie: constant.COOKIE_OPTIONS
+}))
 
 // GET to login page
 router.get('/new', function (req, res, next) {
